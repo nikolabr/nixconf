16e200b34207e767d3d8c124bcee861bfb1f0cdb
nikolabr <nikolabrk@protonmail.com>
2023-11-24 23:59:55 +0100

    Added custom emacs package

--- flake.nix
+++ flake.nix
@@ -31,6 +31,21 @@
   in {
     packages.x86_64-linux.default = home-manager.defaultPackage.x86_64-linux;
 
+    packages.x86_64-linux.custom-emacs =
+      (pkgs.emacsWithPackagesFromUsePackage {
+        config = ./init.el;
+        defaultInitFile = true;
+        alwaysEnsure = true;
+
+        package = pkgs.emacs-gtk;
+                
+        # Optionally provide extra packages not in the configuration file.
+        extraEmacsPackages = epkgs: [
+          epkgs.use-package
+          epkgs.melpaPackages.nix-mode
+        ];
+      });
+    
     homeConfigurations = {
       "nikola" = home-manager.lib.homeManagerConfiguration {
         modules = [
@@ -41,19 +56,7 @@
               defaultEditor = true;
               client.enable = true;
 
-              package = (pkgs.emacsWithPackagesFromUsePackage {
-                config = ./init.el;
-                defaultInitFile = true;
-                alwaysEnsure = true;
-
-                package = pkgs.emacs-gtk;
-
-                # Optionally provide extra packages not in the configuration file.
-                extraEmacsPackages = epkgs: [
-                  epkgs.use-package
-                  epkgs.melpaPackages.nix-mode
-                ];
-              });
+              package = self.packages.x86_64-linux.custom-emacs;
             };
           }
           { home.stateVersion = "23.05"; }
