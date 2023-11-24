6d8ac78a52fb08ceab229b35e344e35be8eab33f
nikolabr <nikolabrk@protonmail.com>
2023-11-24 22:50:24 +0100

    Keep emacs config inside flake

--- flake.nix
+++ flake.nix
@@ -21,24 +21,45 @@
   };
 
   outputs = { self, nixpkgs, home-manager, emacs-overlay, rust-overlay }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays =
-          [ emacs-overlay.overlays.default rust-overlay.overlays.default ];
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays =
+        [ emacs-overlay.overlays.default rust-overlay.overlays.default ];
         config.allowUnfree = true;
-      };
-    in {
-      packages.x86_64-linux.default = home-manager.defaultPackage.x86_64-linux;
+    };
+  in {
+    packages.x86_64-linux.default = home-manager.defaultPackage.x86_64-linux;
 
-      homeConfigurations = {
-        "nikola" = home-manager.lib.homeManagerConfiguration {
-          modules = [
-            ./home.nix
-            { home.stateVersion = "23.05"; }
-          ];
-          inherit pkgs;
-        };
+    homeConfigurations = {
+      "nikola" = home-manager.lib.homeManagerConfiguration {
+        modules = [
+          ./home.nix
+          {
+            services.emacs = {
+              enable = true;
+              defaultEditor = true;
+              client.enable = true;
+
+              package = (pkgs.emacsWithPackagesFromUsePackage {
+                config = ./init.el;
+                defaultInitFile = true;
+                alwaysEnsure = true;
+
+                package = pkgs.emacs-gtk;
+
+                # Optionally provide extra packages not in the configuration file.
+                extraEmacsPackages = epkgs: [
+                  epkgs.use-package
+                  epkgs.melpaPackages.nix-mode
+                ];
+              });
+            };
+          }
+          { home.stateVersion = "23.05"; }
+        ];
+        inherit pkgs;
       };
     };
+  };
 }
--- home.nix
+++ home.nix
@@ -48,26 +48,6 @@
     font.size = 8.0;
   };
 
-  services.emacs = {
-    enable = true;
-    defaultEditor = true;
-    client.enable = true;
-
-    package = (pkgs.emacsWithPackagesFromUsePackage {
-      config = "./init.el";
-      defaultInitFile = true;
-      alwaysEnsure = true;
-
-      package = pkgs.emacs-gtk;
-
-      # Optionally provide extra packages not in the configuration file.
-      extraEmacsPackages = epkgs: [
-        epkgs.use-package
-        epkgs.melpaPackages.nix-mode
-      ];
-    });
-  };
-
   services.polybar.enable = true;
   services.polybar.package = pkgs.polybar.override { pulseSupport = true; };
   services.polybar.script = "polybar top &";
